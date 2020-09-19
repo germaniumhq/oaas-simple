c0f30145d33246260731b6bd1f1588b57d0b2f5b
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-09-19 18:55:28 +0200

    Fixed the repo location

--- _adhesive.py
+++ _adhesive.py
@@ -4,7 +4,7 @@ import germanium_py_exe  # type: ignore
 germanium_py_exe.pipeline(
     {
         "run_mypy": False,
-        "repo": "git@github.com:germaniumhq/oaas_simple.git",
+        "repo": "git@github.com:germaniumhq/oaas-simple.git",
         "binaries": {
             "name": "Python 3.8 on Linux x64",
             "platform": "python:3.8",
