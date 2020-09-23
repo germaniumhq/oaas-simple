016f897d02857b337cfab95618d7547843db0599
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-09-23 15:51:42 +0200

    Generate proto files

--- bin/generate-proto.py
+++ bin/generate-proto.py
@@ -11,6 +11,7 @@ FOLDER = "grpc"
 EXTENSION = ".proto"
 
 IMPORT_RE = re.compile(r"^(import )(.+? as .*)$", re.MULTILINE)
+ASSIGNMENT_RE = re.compile(r"^(def add_)(.+?)(_)(to_server\(servicer, server\):)$")
 
 
 def main() -> None:
@@ -106,13 +107,18 @@ def grpc_compile(
     )
 
     # ####################################################################
-    # Patch the imports because the generator can't do it
-    # https://github.com/protocolbuffers/protobuf/issues/7061
+    # Patch the imports because the generator can't do it, and prepare
+    # for OaaS.
     # ####################################################################
     with open(generated_grpc_file, "rt", encoding="utf-8") as f:
         content = f.read()
 
-    new_content = IMPORT_RE.sub(r"\1 oaas_simple.rpc.\2", content)
+        new_content = IMPORT_RE.sub(r"\1 oaas_simple.rpc.\2", content)
+
+        # In OaaS we register as clients directly the type. Unfortunately,
+        # to add them into a grpc server we need to use the generated
+        # add_to_server
+        new_content = convert_to_static_method(new_content)
 
     with open(generated_grpc_file, "wt", encoding="utf-8") as f:
         f.write(new_content)
@@ -130,6 +136,29 @@ def grpc_compile(
             generated_proto_pyi,
         ]
     )
+
+
+def convert_to_static_method(new_content: str) -> str:
+    lines = new_content.split("\n")
+    index = 0
+
+    while index < len(lines):
+        m = ASSIGNMENT_RE.match(lines[index])
+
+        if not m:
+            index += 1
+            continue
+
+        lines[index] = m.group(1) + m.group(4)
+        lines.insert(index, "@staticmethod")
+
+        while lines[index]:
+            lines[index] = "    " + lines[index]
+            index += 1
+
+        index += 1
+
+    return "\n".join(lines)
 
 
 def get_last_modified(file_name: str) -> int:
